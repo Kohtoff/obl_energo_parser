651683e5ab48c98d4d2e261dfeb9be353c908723
Kohtov Danylo <danil.kohtoff@gmail.com>
2022-01-14 14:53:22 +0200

    Added function getDataByDate

--- index.js
+++ index.js
@@ -28,23 +28,58 @@ const parse = async () => {
     '.quest_block:has(.questing:contains("м. Запоріжжя (Запорізькі міські електричні мережі)"))'
   );
 
-  let dates = [];
   /*search all dates, when works are planned */
-  targetCity.find("td[colspan=5] > span > strong").each((index, el) => {
-    dates.push($(el).text());
-  });
+  const getDates = (targetCity) => {
+    let dates = [];
 
-  /*formating date for comfortable compare */
-  const formatDate = (date) => date.split('.').reverse().join('.');
-  const today = formatDate(new Date().toLocaleDateString())
-  /*filter to save only futures dates */
-  dates = dates.filter((date) => {
-    if (formatDate(date) < today) return false;
-    return true;
-  });
-  console.log(dates);
+    targetCity.find("td[colspan=5] > span > strong").each((index, el) => {
+      dates.push($(el).text());
+    });
 
-  /*TODO: Parse addresses depending on dates. Parse reason of blackout depending on addresses */
+    /*formating date for comfortable compare */
+    const formatDate = (date) => date.split(".").reverse().join(".");
+    const today = formatDate(new Date().toLocaleDateString());
+    /*filter to save only futures dates */
+    dates = dates.filter((date) => {
+      if (formatDate(date) < today) return false;
+      return true;
+    });
+    return dates;
+  };
+
+  /*get info for each blackout of the date */
+  const getDataByDate = (date) => {
+    /*Start value is next to date row */
+    let currentRow = targetCity.find(`td:Contains(${date}) < tr`).next();
+    /*array to store the results of parse */
+    const result = [];
+    /*while row`s text hasn`t structure as dd.mm.yyyy */
+    while (!currentRow.text().match(/\d{2}\.\d{2}\.\d{4}/)) {
+      /*array of formated data*/
+      /*delete sapces before and after string */
+      /*splitting string by special symbols of new line */
+      /*The same actions for each element in array */
+      const recievedData = currentRow.text()
+        .trim()
+        .split('\n\n')
+        .map(item => item.replace('\n', '').trim())
+      const blackoutData = {
+        time: recievedData[0],
+        city: recievedData[1],
+        addresses: recievedData[2].split('\n'),
+        reason: recievedData[3],
+      };
+      result.push(blackoutData)
+      currentRow = currentRow.next();
+    }
+    return result
+  };
+
+  const parsedInfo = getDates(targetCity).reduce((obj, key) => ({...obj, [key]: getDataByDate(key)}))
+  console.log(parsedInfo);
+
+
+  return parsedInfo
 };
 
 parse();
